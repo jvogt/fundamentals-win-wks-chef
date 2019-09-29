d14f6056bc89e2975a4926aeafab35403029fa70
Jeff Vogt <jvogt@chef.io>
2019-09-29 13:41:45 -0700

    do real stuff

--- recipes/default.rb
+++ recipes/default.rb
@@ -8,3 +8,16 @@ file 'c:/hello.txt' do
   content 'Hello Fundamentals Class!'
   action :create
 end
+
+%w(googlechrome atom putty git cmder chefdk).each do |p|
+  chocolatey_package p
+end
+
+directory 'c:/workshop' do
+  action :create
+end
+
+git 'c:/workshop/zzz-examples' do
+  action :sync
+  repository 'https://github.com/jvogt/fundamentals-examples'
+end
