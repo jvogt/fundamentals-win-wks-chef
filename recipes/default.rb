7188d91fb78deb28e0ea69c72ff614c4325014f7
Jeff Vogt <jvogt@chef.io>
2019-09-29 17:09:06 -0700

    do real stuff

--- recipes/default.rb
+++ recipes/default.rb
@@ -21,3 +21,20 @@ git 'c:/workshop/zzz-examples' do
   action :sync
   repository 'https://github.com/jvogt/fundamentals-examples'
 end
+
+directory 'c:/users/administrator/.chef' do
+  action :create
+  recursive true
+end
+
+file 'c:/users/administrator/.chef/user.pem' do
+  action :create
+  content node['chef_client_key']
+end
+
+file 'c:/users/administrator/.chef/config.rb' do
+  action :create
+  content "chef_server_url: '#{node['chef_server_url']}
+node_name: '#{node['workstation_user']}'
+client_key: File.dirname(__FILE__) + '/user.pem'
+"
